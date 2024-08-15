2aeb221172f407b4c9591c28cd9a6eb04fb83049
unknown <Administrador@LABF530128.fuvs.br>
2024-08-15 19:47:26 -0300

    exercicio resolvido

--- express/index.js
+++ express/index.js
@@ -1,16 +1,30 @@
 const express = require("express")
 const app = express()
-// rota raiz  // Lorem = ctrl + shift + p
 app.get('/', (req, res) => {
+    res.send("<h1>Página principal</h1>")
+})
+// rota raiz  // Lorem = ctrl + shift + p
+app.get('/contato', (req, res) => {
     const html = `
-        <form>
+        <form method='get'>
+            <div>
+                <label>Nome</label><br> 
+                <input name='nome' type='text' />
+            </div>
+            <div>
+                <label>Mensagem</label><br>
+                <textarea name='mensagem' cols='20' rows='5'></textarea>
+            </div>
+            <div><button type='submit'>Enviar</button></div>
         </form>
     `
-    res.send("<strong>Minha primeira página com express</strong>")
+    res.send(html)
 })
 
 app.get('/sobre', (req, res) => {
-    res.send("Sobre o site")
+    const lorem = `Tempor voluptate consectetur adipisicing velit anim. Exercitation amet sunt magna aliqua dolore dolor culpa non minim officia sit. Excepteur nostrud aute nulla adipisicing officia ullamco officia dolore ad sit pariatur consequat ipsum nostrud. Aliqua ullamco in aliqua sint tempor elit deserunt culpa est est. Cillum Lorem reprehenderit aliquip voluptate consequat.
+                    Ea excepteur reprehenderit aute voluptate mollit. Cillum laborum amet elit aliqua irure mollit labore ipsum ut reprehenderit ex velit ut. Enim consequat occaecat reprehenderit ullamco consequat tempor. Aliqua anim laborum ad sunt aliqua officia cupidatat laborum eu exercitation adipisicing reprehenderit.`
+    res.send(lorem)
 })
 
 app.listen(3000, () => {
